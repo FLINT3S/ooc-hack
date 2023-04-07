8947c7c7e0e6ce0655f98f8ec2f5eff9a6972b00
Philipoff <Filippok2002@yandex.ru>
2023-04-07 22:46:00 +0300

    strapi database create entity

--- server/src/services/strapi/database.py
+++ server/src/services/strapi/database.py
@@ -9,13 +9,19 @@ async def update_entity(collection, field, data):
     strapi = StrapiClient(strapi_url)
     await strapi.authorize(login, password) # optional
     users = await strapi.get_entries(collection, filters={field: {'$eq': data}})
-    print(users)
-    user_id = users["data"][0]['id']
-    print(user_id)
-    r = await strapi.update_entry(collection, user_id, data={field: 'admin'})
-    print(r)
-    users = await strapi.get_entries(collection, filters={field: {'$eq': data}})
-    print(users)
+    try:
+        user_id = users["data"][0]['id']
+        await strapi.update_entry(collection, user_id, data={field: 'admin'})
+    except IndexError as e:
+        print("No data with your properties")
 
 
-asyncio.run(update_entity("tests", "test", "vasysa"))
+async def create_entity(collection, data):
+    strapi = StrapiClient(strapi_url)
+    await strapi.authorize(login, password) # optional
+    await strapi.create_entry(collection, {"test": data})
+
+
+if __name__ == "__main__":
+    asyncio.run(update_entity("tests", "test", "vasysa"))
+    asyncio.run(create_entity("tests", "vasya_new"))
