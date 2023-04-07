0b77f1f1f142e8c71ad4aee5f8fd93eb4ab4b98a
Philipoff <Filippok2002@yandex.ru>
2023-04-08 01:11:32 +0300

    feature<server>: strapi get entity

--- server/src/services/strapi/database.py
+++ server/src/services/strapi/database.py
@@ -1,9 +1,14 @@
 import asyncio
+import os
 from strapi_client import StrapiClient
 
-strapi_url = "https://2198-178-71-66-26.eu.ngrok.io/"
-login = "ooc-hack@flint3s.ru"
-password = "Sadness9-Judiciary-Fraternal"
+# strapi_url = "https://2198-178-71-66-26.eu.ngrok.io/"
+# login = "ooc-hack@flint3s.ru"
+# password = "Sadness9-Judiciary-Fraternal"
+
+strapi_url = os.environ['STRAPI_URL']
+login = os.environ['STRAPI_LOGIN']
+password = os.environ['STRAPI_PASSWORD']
 
 async def update_entity(collection, field, data):
     strapi = StrapiClient(strapi_url)
@@ -22,6 +27,18 @@ async def create_entity(collection, data):
     await strapi.create_entry(collection, {"test": data})
 
 
+async def get_entity(collection, field, data):
+    strapi = StrapiClient(strapi_url)
+    await strapi.authorize(login, password) # optional
+    data = await strapi.get_entries(collection, filters={field: {'$eq': data}})
+    data = data["data"]
+    if data:
+        return data[0]
+    return None
+
+
 if __name__ == "__main__":
-    asyncio.run(update_entity("tests", "test", "vasysa"))
-    asyncio.run(create_entity("tests", "vasya_new"))
+    # asyncio.run(update_entity("tests", "test", "vasysa"))
+    # asyncio.run(create_entity("tests", "vasya_new"))
+    test_object = asyncio.run(get_entity("tests", "test", "admin"))
+    print(test_object)
