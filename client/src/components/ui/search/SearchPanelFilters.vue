86c194da3dc796fd080f43d5e38d87674e929fcf
siailya <sia.ilya.2017@gmail.com>
2023-04-09 00:36:05 +0300

    feature<client>: Главная, компонент поиска, тема

---
+++ client/src/components/ui/search/SearchPanelFilters.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="d-flex justify-content-between mt-3">
+        <div class="d-flex">
+            <span>
+                Поиск по:&nbsp;
+            </span>
+
+            <n-popselect v-model:value="searchData.searchType" :options="options" trigger="click">
+                <div class="d-flex align-items-center cursor-pointer">
+                <span class="text-accent">
+                    {{ searchType.label }}
+                </span>
+                    <svg class="ms-1" fill="var(--accent-red)" height="5" style="" viewBox="0 0 10 5" width="8"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0L5 5L10 0H0Z"/>
+                    </svg>
+                </div>
+            </n-popselect>
+        </div>
+
+        <search-advanced-filters v-if="searchType.value === 'realty'" :search-data="searchData"/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import SearchAdvancedFilters from "@components/ui/search/SearchAdvancedFilters.vue";
+import {SearchDataType} from "@data/types/searchData";
+
+const options = [
+    {
+        label: 'объектам недвижимости',
+        value: 'realty'
+    },
+    {
+        label: 'поручениям',
+        value: 'tasks'
+    }
+]
+
+interface Props {
+    searchData: SearchDataType
+}
+
+const props = defineProps<Props>()
+
+const searchType = computed(() => {
+    return options.find((o) => o.value === props.searchData.searchType)
+})
+</script>
---
+++ client/src/components/ui/widgets/DashboardNumber.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="d-flex align-items-center">
+        <slot>
+            <svg fill="none" height="26" style="" viewBox="0 0 30 26" width="30"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.9999 5.98669L25.0399 23.3333H4.95992L14.9999 5.98669ZM14.9999 0.666687L0.333252 26H29.6666L14.9999 0.666687ZM16.3333 19.3333H13.6666V22H16.3333V19.3333ZM16.3333 11.3334H13.6666V16.6667H16.3333V11.3334Z"
+                      fill="#696969"/>
+            </svg>
+        </slot>
+
+        <div class="ms-3">
+            <p class="text-secondary">{{ title }}</p>
+            <span>{{ number }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    title: string,
+    number: string | number
+}
+
+const props = defineProps<Props>()
+</script>
