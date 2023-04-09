b925e852aaa3d3606b94ef653a10bb81bccc9e57
siailya <sia.ilya.2017@gmail.com>
2023-04-10 01:03:54 +0300

    feature<strapi>: История задач

---
+++ service/src/api/task-history/controllers/task-history.ts
@@ -0,0 +1,7 @@
+/**
+ * task-history controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::task-history.task-history');
---
+++ service/src/api/task-history/routes/task-history.ts
@@ -0,0 +1,7 @@
+/**
+ * task-history router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::task-history.task-history');
