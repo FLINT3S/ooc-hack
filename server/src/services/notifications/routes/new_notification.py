ce2f8b28bb6f020f06696eb7178ac8782e0af0a3
Philipoff <Filippok2002@yandex.ru>
2023-04-09 05:39:49 +0300

    fix<server>: drop server fix

--- server/src/services/notifications/routes/new_notification.py
+++ server/src/services/notifications/routes/new_notification.py
@@ -7,7 +7,7 @@ notifications_router = APIRouter()
 
 base_url = "https://585c-185-26-73-71.ngrok-free.app"
 @notifications_router.post("/new_notification")
-async def new_meeting(request: Request, response: Response):
+async def new_meeting(request: Request):
     payload = await request.body()
     requests_send.post(base_url + "/notifications/new_notification", data={"payload": payload})
     return {"status": "OK"}
