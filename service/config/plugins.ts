3748ecd8577ee83680bdc5af24aedf47d067ecb0
siailya <sia.ilya.2017@gmail.com>
2023-04-10 04:58:29 +0300

    feature<strapi>: Увеличен рейтлимит

--- service/config/plugins.ts
+++ service/config/plugins.ts
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
   // ...
+  "users-permissions": {
+    config: {
+      ratelimit: {
+        interval: 60000,
+        max: 100000
+      }
+    }
+  },
 
   "fuzzy-search": {
     enabled: true,
