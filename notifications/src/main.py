fe20d6e1274d5adeed44b6e4b5286b1fbd6e9e97
Philipoff <Filippok2002@yandex.ru>
2023-04-10 05:44:31 +0300

    feature<notifications>: added work group to notification

--- notifications/src/main.py
+++ notifications/src/main.py
@@ -23,6 +23,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+statuses = {
+    "pending": 'Ожидает',
+    "in_progress": 'В работе',
+    "review": 'На проверке',
+    "need_correction": 'Требуется уточнение',
+    "done": 'Завершено'
+}
+
 
 class Data(BaseModel):
     data: str
@@ -33,7 +41,6 @@ async def root(data=Body()):
     data = data.decode('utf-8')  # bytes to str
     data = (unquote(data))[8:]  # url string to string and slice key "payload="
     data = json.loads(data)  # str to json
-    print(data)
     if data["model"] == "meeting" and data["event"] == "entry.create":  # Уведомление при создании новой встречи
         meeting_info = await get_meeting(data["entry"]["id"])
         print(meeting_info)
@@ -57,11 +64,11 @@ async def root(data=Body()):
                     for client in work_group["attributes"]["clients"]["data"]: # Send message in Telegram
                         await bot.send_message(client["attributes"]["telegramId"], text)
     elif data["model"] == "task" and data["event"] == "entry.update":
-        text = f"Обновлён статус решения \"{data['entry']['title']}\" на \"{data['entry']['status']}\""
         task = await get_task(data["entry"]["id"])
         if task["attributes"]["workGroups"]["data"]:
             for work_group in task["attributes"]["workGroups"]["data"]:
                 work_group = await get_users_from_working_group(work_group["id"])
+                text = f"Обновлён статус решения \"{data['entry']['title']}\"({work_group['attributes']['title']}) на \"{statuses[data['entry']['status']]}\""
                 await create_notification({
                     "workGroup": work_group,
                     "title": "Task status changed",
